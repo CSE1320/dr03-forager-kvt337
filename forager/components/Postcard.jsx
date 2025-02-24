5a0eeb08016dcfdf4b1f4a2946662f55d9641a88
kvt337 <123342243+kvt337@users.noreply.github.com>
2025-02-24 08:11:15 +0000

    finished postcard component

--- forager/components/Postcard.jsx
+++ forager/components/Postcard.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+//import {mushroomData} from '../data/development';
 
-const Postcard = ({image, name}) => {
+const Postcard = ({image, name, warning}) => {
     return (
-        <div className="polaroid relative w-24 h-32 bg-white shadow-[3px_3px_3px_2.5px_rgba(0,0,0,0.1)]">
-            {/* <div class="PolaroidCard">
-                <div class="Polaroid">
-                    <img class="" src="assets/death_cap.png">
-                    <img class="warning" src="assets/icon_warning.png">
+        <li className="postcard m-2.5 flex flex-col items-center">
+            <div className="polaroid-card relative w-24 h-32 bg-white shadow-[3px_3px_3px_2.5px_rgba(0,0,0,0.1)] ">
+                <div className="polaroid flex flex-wrap justify-center">
+                    <img className="mt-1" src={image} alt="mushroom image"/>
+                    {warning ? (
+                    <img className="warning absolute left-2.5 top-1.25" src={warningIcon} alt="warning icon"/>
+                    ) : null}
                 </div>
             </div>
-            <div class="Title"><p>Death Cap</p></div> */}
-        </div>
+            <div className="title mb-0.5 text-[#203B5F] text-center font-nunito text-[16px] font-semibold leading-normal">
+                <p>{name}</p>
+            </div>
+        </li>
     );
 };
 
--- forager/data/development.jsx
+++ forager/data/development.jsx
@@ -10,9 +10,20 @@ const warningMessage = {
     message: "This is a toxic species, proceed with caution."
 };
 
+const mushroomData = [
+    {
+        name: "Death Cap",
+        image: "/images/deathcap.jpg",
+        warning: true,
+    }
+];
+
+export default mushroomData;
+
+
 
 // Two flavors of exporting:
 // export default dummyData; // Requires import dummyData from './data/development.js';
 
 // More than one export.
-export {warningMessage, dummyData}; // Requires import {warningMessage, dummyData} from './data/development.js';
+export {warningMessage, dummyData, mushroomData}; // Requires import {warningMessage, dummyData} from './data/development.js';
